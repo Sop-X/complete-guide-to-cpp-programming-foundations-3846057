09087f5c8ac897a0cace38d37df22b402dcdf8f4
Sop-X <147946660+Sop-X@users.noreply.github.com>
2026-01-07 23:12:49 +0000

    Calculate Average

--- src/Ch03/03_08/CodeDemo.cpp
+++ src/Ch03/03_08/CodeDemo.cpp
@@ -8,7 +8,7 @@
 double my_average(int a, int8_t b, uint32_t c){
     double result = 0.0;
 
-    // Write your code here
+    result = (static_cast<double>(a) + static_cast<double>(b) + static_cast<double>(c)) / 3.0;
     
     return result;
 }
